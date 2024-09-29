ffb2b9260bc3418d5b3c328f540cc61ec7269c53
royeradames <raadamesbonilla@pplweb.com>
2024-09-29 17:58:38 -0400

    simplify

--- storyblok/_components/CmsParonamaImage.tsx
+++ storyblok/_components/CmsParonamaImage.tsx
@@ -1,11 +1,9 @@
 import { SbImage } from "@/storyblok/types/SbImage";
 import NextImage from "next/image";
 import { storyblokEditable } from "@storyblok/react/rsc";
-import { formatTitleToId } from "@/app/(helpers)/formatTitleToId";
 
 export type TCmsPanoramaImage = {
   blok: {
-    title: string;
     image: SbImage;
   };
 };
@@ -13,17 +11,16 @@ export type TCmsPanoramaImage = {
 export default function CmsPanoramaImage({
   blok,
   blok: {
-    title,
-    image: { alt, filename },
+    image: { filename },
   },
 }: TCmsPanoramaImage) {
   return (
-    <section
+    <div
+      data-component="CmsPanoramaImage"
       {...storyblokEditable(blok)}
-      aria-label={formatTitleToId(title)}
       className="grid mt-20"
     >
       <NextImage alt="" src={filename} width={1280} height={358} />;
-    </section>
+    </div>
   );
 }
