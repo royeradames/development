6af610ba98e7bfaba4b8c6c337988b10720d9dc0
royeradames <raadamesbonilla@pplweb.com>
2024-09-29 14:29:46 -0400

    fix mobile view

--- storyblok/_components/CmsGalleryGrid.tsx
+++ storyblok/_components/CmsGalleryGrid.tsx
@@ -15,7 +15,7 @@ export function CmsGalleryGrid({
   const Section = title ? "section" : "div";
   return (
     <Section
-      className="mt-20 px-4 lg:px-0 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 "
+      className="gap-6 mt-20 px-4 lg:px-0 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
       {...storyblokEditable(blok)}
     >
       {title ? (
