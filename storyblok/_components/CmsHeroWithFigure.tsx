872fa72cfa288b87aaa308fc80cb2226bd295051
royeradames <raadamesbonilla@pplweb.com>
2024-09-30 19:25:50 -0400

    fix: missing key, missing section name

--- storyblok/_components/CmsHeroWithFigure.tsx
+++ storyblok/_components/CmsHeroWithFigure.tsx
@@ -2,6 +2,7 @@ import { SbImage } from "@/storyblok/types/SbImage";
 import NextImage from "next/image";
 import { storyblokEditable } from "@storyblok/react/rsc";
 import { ISbRichtext } from "@storyblok/react";
+import { formatTitleToId } from "@/app/(helpers)/formatTitleToId";
 
 export type TCmsHeroWithFigure = {
   blok: {
@@ -21,9 +22,11 @@ export function CmsHeroWithFigure({
     content,
   },
 }: TCmsHeroWithFigure) {
+  const headingId = formatTitleToId(title);
   return (
-    <article
-      className="mt-20 px-4 xl:px-0 max-w-7xl mx-auto grid md:grid-cols-2 gap-12 items-center"
+    <section
+      className="my-20 px-4 xl:px-0 max-w-7xl mx-auto grid md:grid-cols-2 gap-12 items-center"
+      aria-labelledby={headingId}
       {...storyblokEditable(blok)}
     >
       <figure>
@@ -33,15 +36,19 @@ export function CmsHeroWithFigure({
         </figcaption>
       </figure>
       <div className="flex flex-col gap-4">
-        <h2 className="text-5xl font-bold text-white">{title}</h2>
+        <h2 className="text-5xl font-bold text-white" id={headingId}>
+          {title}
+        </h2>
         {content.content?.map((aParagraph) => {
           return aParagraph.content?.map((aParagraphData) => {
             return (
-              <p className="text-base text-white">{aParagraphData.text}</p>
+              <p className="text-base text-white" key={aParagraphData.text}>
+                {aParagraphData.text}
+              </p>
             );
           });
         })}
       </div>
-    </article>
+    </section>
   );
 }
