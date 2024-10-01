e7b551b5bf19b9841305f727c8a5c2337b52dbfd
royeradames <raadamesbonilla@pplweb.com>
2024-10-01 12:06:25 -0400

    missing features

--- storyblok/_components/CmsFullBleedCarousel.tsx
+++ storyblok/_components/CmsFullBleedCarousel.tsx
@@ -1,52 +1,54 @@
 export type TFullBleedCarousel = {
-  blok: {};
+  blok: {
+    title?: string;
+  };
 };
 
-export function CmsFullBleedCarousel({ blok }: TFullBleedCarousel) {
+export function CmsFullBleedCarousel({
+  blok,
+  blok: { title = "Case studies" },
+}: TFullBleedCarousel) {
   return (
-    <div className="carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4">
-      <div className="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-          className="rounded-box"
-        />
+    <section className="py-20 bg-white">
+      {title ? (
+        <h2 className="text-5xl font-bold mb-10 text-black px-4 xl:px-0 max-w-lg mx-auto">
+          {title}
+        </h2>
+      ) : undefined}
+      {/* todo: should cover the whole width and be show side items */}
+
+      <div className="carousel carousel-center w-full pb-4 space-x-4 max-w-lg">
+        <div className="carousel-item">
+          <img
+            id="slide1"
+            src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+            className="rounded-box "
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            id="slide2"
+            src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+            className="rounded-box "
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            id="slide3"
+            src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+            className="rounded-box "
+          />
+        </div>
       </div>
-      <div className="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-          className="rounded-box"
-        />
+      <div className="flex gap-6 px-4 xl:px-0 max-w-7xl mx-auto">
+        {/* todo: navigation should be smooth and it shouldn't hide the title */}
+        <a href="#slide1" className="btn btn-circle">
+          ❮
+        </a>
+        <a href="#slide3" className="btn btn-circle">
+          ❯
+        </a>
       </div>
-      <div className="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-          className="rounded-box"
-        />
-      </div>
-      <div className="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-          className="rounded-box"
-        />
-      </div>
-      <div className="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
-          className="rounded-box"
-        />
-      </div>
-      <div className="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
-          className="rounded-box"
-        />
-      </div>
-      <div className="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-          className="rounded-box"
-        />
-      </div>
-    </div>
+    </section>
   );
 }
