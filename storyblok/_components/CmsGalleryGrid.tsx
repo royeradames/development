760fb3d160c98d2b3caff320db925d02a7dba4fa
royeradames <raadamesbonilla@pplweb.com>
2024-10-01 11:33:14 -0400

    CmsFullBleedCarousel setup and template

--- storyblok/_components/CmsGalleryGrid.tsx
+++ storyblok/_components/CmsGalleryGrid.tsx
@@ -1,29 +1,69 @@
 import { SbBlokData } from "@storyblok/react";
 import { StoryblokComponent, storyblokEditable } from "@storyblok/react/rsc";
+import { clsx } from "clsx";
+import { formatTitleToId } from "@/app/(helpers)/formatTitleToId";
 
 export type TCmsGalleryGrid = {
   blok: {
     title?: string;
     items: SbBlokData[];
+    /**
+     * Manual contrast of the component
+     * Can be changed to style
+     */
+    isDarkMode: boolean;
+    /**
+     * determines how many columns out of 4.
+     */
+    layout: "ascending" | "odds";
   };
 };
 
 export function CmsGalleryGrid({
   blok,
-  blok: { title, items },
+  blok: { title, items, isDarkMode = true, layout = "ascending" },
 }: TCmsGalleryGrid) {
   const Section = title ? "section" : "div";
+  const headingId = title ? formatTitleToId(title) : "";
   return (
     <Section
-      className="max-w-7xl mx-auto gap-6 mt-20 px-4 xl:px-0 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+      className={clsx("", {
+        "bg-white": !isDarkMode,
+        "bg-black": isDarkMode,
+      })}
+      aria-labelledby={title ? headingId : undefined}
       {...storyblokEditable(blok)}
     >
-      {title ? (
-        <h2 className="text-5xl font-bold col-span-full mb-10">{title}</h2>
-      ) : undefined}
-      {items.map((item) => {
-        return <StoryblokComponent blok={item} key={item._uid} />;
-      })}
+      <div
+        className={clsx("max-w-7xl mx-auto gap-6 py-20 px-4 xl:px-0 grid ", {
+          "bg-white": !isDarkMode,
+          "bg-black": isDarkMode,
+          "sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4":
+            layout === "ascending",
+          "grid-cols-2 lg:grid-cols-4": layout === "odds",
+        })}
+      >
+        {title ? (
+          <h2
+            id={headingId}
+            className={clsx("text-5xl font-bold col-span-full mb-10", {
+              "text-white": isDarkMode,
+              "text-black": !isDarkMode,
+            })}
+          >
+            {title}
+          </h2>
+        ) : undefined}
+        {items.map((item) => {
+          return (
+            <StoryblokComponent
+              isDarkMode={isDarkMode}
+              blok={item}
+              key={item._uid}
+            />
+          );
+        })}
+      </div>
     </Section>
   );
 }
