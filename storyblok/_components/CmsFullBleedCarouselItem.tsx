ece63ab67123e5e1c647cb3fb9edb2e4eda96b09
royeradames <raadamesbonilla@pplweb.com>
2024-10-05 13:01:10 -0400

    allow background images

--- storyblok/_components/CmsFullBleedCarouselItem.tsx
+++ storyblok/_components/CmsFullBleedCarouselItem.tsx
@@ -1,8 +1,18 @@
-import { StoryblokComponent, storyblokEditable } from "@storyblok/react/rsc";
+import { storyblokEditable } from "@storyblok/react/rsc";
 import { clsx } from "clsx";
 import { SbImage } from "@/storyblok/types/SbImage";
-import { SbBlokData } from "@storyblok/react";
 import { formatTitleToId } from "@/app/(helpers)/formatTitleToId";
+import Link from "next/link";
+import NextImage from "next/image";
+
+export type SbLink = {
+  id: string;
+  url: string;
+  linktype: "url";
+  fieldtype: "multilink" | "story";
+  cached_url: string;
+  target: "_blank" | "_self";
+};
 
 type TFullBleedCarouselItem = {
   blok: {
@@ -10,9 +20,11 @@ type TFullBleedCarouselItem = {
     content: string;
     image: SbImage;
     backgroundImage?: SbImage;
-    actions: [SbBlokData];
     backgroundColor: "blue" | "grey" | "green" | "yellow" | "black";
     figure: SbImage;
+    link: SbLink;
+    linkIcon?: SbImage;
+    bgPosition: "bottomRight" | "cover" | "topRight" | "bottom";
   };
   className?: string;
 };
@@ -23,23 +35,46 @@ export function CmsFullBleedCarouselItem({
     title = "Case studies",
     image: { filename, alt },
     content,
-    actions: [action] = [],
     backgroundColor = "black",
     backgroundImage: { filename: backgroundImagefilename } = { filename: "" },
     figure: { filename: figureFilename, alt: figureAlt },
+    link: { url, target },
+    linkIcon: { filename: linkFilename } = { filename: "" },
+    bgPosition,
   },
   className = "",
 }: TFullBleedCarouselItem) {
   const headingId = formatTitleToId(title);
+  const linearGradiant = {
+    blue: "linear-gradient(#015baa,#017ac2)",
+    grey: "",
+    green: "linear-gradient(#007730, #5fc427)",
+    yellow: "",
+    black: "",
+  };
+  // const backgroundImage = `url('${backgroundImagefilename}`;
+  const backgroundImage = `url('${backgroundImagefilename}')${linearGradiant[backgroundColor] ? `, ${linearGradiant[backgroundColor]}` : ""}`;
+  console.log(backgroundImage);
   return (
     <section
       aria-labelledby={headingId}
-      className={clsx("hero bg-base-200 min-h-screen bg-right-bottom", {
-        [className]: className,
-        "bg-[#087d30]": backgroundColor === "green",
-      })}
+      className={clsx(
+        ["hero min-h-screen", "pt-20 lg:pt-0", "items-start lg:items-center"],
+        {
+          [className]: className,
+          "bg-[#015baa]": backgroundColor === "blue",
+          "bg-[#222424]": backgroundColor === "grey",
+          "bg-[#087d30]": backgroundColor === "green",
+          "bg-[#ffdb00]": backgroundColor === "yellow",
+          "bg-black": backgroundColor === "black",
+          "bg-right-bottom": bgPosition === "bottomRight",
+          "bg-cover": bgPosition === "cover",
+          "bg-right-top": bgPosition === "topRight",
+          "bg-bottom": bgPosition === "bottom",
+        },
+      )}
       style={{
-        backgroundImage: `url('${backgroundImagefilename}'), linear-gradient(#007730, #5fc427)`,
+        backgroundImage,
       }}
       {...storyblokEditable(blok)}
     >
@@ -51,11 +86,26 @@ export function CmsFullBleedCarouselItem({
         />
         <div className="flex flex-col gap-6">
           <img src={filename} alt={alt} className="max-w-md" />
-          <h2 id={headingId} className="text-5xl font-bold">
+          <h2 id={headingId} className="text-5xl font-bold text-white">
             {title}
           </h2>
-          <p className="py-6">{content}</p>
-          <StoryblokComponent blok={action} />
+          <p className="text-base text-white">{content}</p>
+          <Link
+            href={url}
+            target={target}
+            className="text-white text-xl font-bold"
+          >
+            Learn more{" "}
+            {linkFilename ? (
+              <NextImage
+                src={linkFilename}
+                alt=""
+                height={29}
+                width={29}
+                className="inline align-middle"
+              />
+            ) : undefined}
+          </Link>
         </div>
       </div>
     </section>
