ceca06b3ea06bb6fa5497cc74dd44ee8737ef53b
royeradames <raadamesbonilla@pplweb.com>
2024-10-09 21:38:01 -0400

    next previous button inside items

--- storyblok/_components/CmsFullBleedCarousel.tsx
+++ storyblok/_components/CmsFullBleedCarousel.tsx
@@ -5,12 +5,13 @@ export type TFullBleedCarousel = {
   blok: {
     title?: string;
     items: SbBlokData[];
+    usePreviousAndNextInsideCard: boolean;
   };
 };
 
 export function CmsFullBleedCarousel({
   blok,
-  blok: { title = "", items },
+  blok: { title = "", items, usePreviousAndNextInsideCard = false },
 }: TFullBleedCarousel) {
   return (
     <section
@@ -25,13 +26,31 @@ export function CmsFullBleedCarousel({
 
       <div className="carousel carousel-center w-full pb-4 space-x-4">
         {items.map((item, index) => {
+          const PreviousAndNextInsideCard = () => {
+            const prevIndex = index === 0 ? items.length - 1 : index - 1;
+            const nextIndex = index === items.length - 1 ? 0 : index + 1;
+
+            return (
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href={`#slide${prevIndex + 1}`} className="btn btn-circle">
+                  ❮
+                </a>
+                <a href={`#slide${nextIndex + 1}`} className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            );
+          };
           return (
-            <div className="carousel-item w-[80%] xl:w-auto">
+            <div className="carousel-item relative w-[80%] xl:w-auto">
               <StoryblokComponent
                 id={`slide${index + 1}`}
                 blok={item}
                 key={item._uid}
               />
+              {usePreviousAndNextInsideCard ? (
+                <PreviousAndNextInsideCard />
+              ) : undefined}
             </div>
           );
         })}
