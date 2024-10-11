6052851891afe699ef804353e1b06b7591ee0a12
royeradames <raadamesbonilla@pplweb.com>
2024-10-11 16:33:31 -0400

    remove lib folder

--- storyblok/_components/CmsFullBleedCarousel.tsx
+++ storyblok/_components/CmsFullBleedCarousel.tsx
@@ -1,7 +1,7 @@
 import {StoryblokComponent, storyblokEditable} from "@storyblok/react/rsc";
 import {SbBlokData} from "@storyblok/react";
 import React, {useEffect, useRef, useState} from "react";
-import {cn} from "@/lib/utils";
+import {cn} from "@/utils/cn";
 
 /**
  * Custom hook for carousel logic.
