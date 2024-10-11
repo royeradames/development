baa8463eee4c49822151e7f1ff459289371cc178
royeradames <raadamesbonilla@pplweb.com>
2024-10-11 16:08:46 -0400

    setup production environment

--- utils/fetchStory.tsx
+++ utils/fetchStory.tsx
@@ -1,11 +1,20 @@
-import { getStoryblokApi } from "@storyblok/react";
+import { getStoryblokApi, ISbResult } from "@storyblok/react";
 
 export const fetchStory = async (storyName: string) => {
   const storyblokApi = getStoryblokApi();
-  const response = await storyblokApi.get(
-    `cdn/stories/${storyName}`,
-    { version: "draft" },
-    { cache: "no-store" },
-  );
+  let response: ISbResult;
+  try {
+    const version: "draft" | "published" =
+      process.env.ENVIRONMENT === "local" ? "draft" : "published";
+    const cache: "no-store" | "default" =
+      process.env.ENVIRONMENT === "local" ? "no-store" : "default";
+    response = await storyblokApi.get(
+      `cdn/stories/${storyName}`,
+      { version },
+      { cache },
+    );
+  } catch (e) {
+    throw Error("Response Storyblok Error");
+  }
   return response.data.story;
 };
